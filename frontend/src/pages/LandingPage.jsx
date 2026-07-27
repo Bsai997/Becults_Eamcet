cb87cbfb04eb4299e06471614e08e490646a8f93
Bommi venkata sai <bommisai23@gmail.com>
2026-07-28 01:14:32 +0530

    landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import Header from '../components/landing/Header';
-import Footer from '../components/landing/Footer';
-import HeroSection from '../components/landing/HeroSection';
-import WhatsAppButton from '../components/landing/Whatsappbtn';
-import { useEffect } from 'react';
+import React, { useEffect } from "react";
+import Header from "../components/landing/Header";
+import Footer from "../components/landing/Footer";
+import HeroSection from "../components/landing/HeroSection";
+import WhatsAppButton from "../components/landing/Whatsappbtn";
+
 export default function LandingPage() {
   useEffect(() => {
-    window.gtag('event', 'landing_page');
+    if (window.gtag) {
+      window.gtag("event", "landing_page");
+    }
   }, []);
+
   return (
     <div className="w-full overflow-x-hidden">
       <Header />
-      <HeroSection/>
+      <HeroSection />
       <WhatsAppButton />
       <Footer />
     </div>
